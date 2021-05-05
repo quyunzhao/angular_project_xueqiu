877b87dd610bfce74dd2654eaa04ccd5bda6eba0
jingqu <1264352733@qq.com>
2021-05-05 21:00:14 +0800

    编写服务器

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 雪球股票
 
 server 服务器目录
+-- cd server
+-- nodemon index.js 启动后台服务 全局安装 nodemon（npm i nodemon -g）
--- server/index.js
+++ server/index.js
@@ -4,6 +4,17 @@ const axios = require("axios");
 
 const app = express();
 
+// 监听端口
+const port = 8080;
+
 app.get("/", (req, res) => {
-  res.send();
+  res.send("apiServer");
+});
+
+app.get("/api/index", (req, res) => {
+  res.json("apiServer");
+});
+
+app.listen(port, () => {
+  console.log("server start", "http://localhost:8080");
 });
