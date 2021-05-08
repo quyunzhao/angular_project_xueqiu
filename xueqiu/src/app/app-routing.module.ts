ded1249ab6b68d400e69fb752d17fcbf3146cc2f
jingqu <1264352733@qq.com>
2021-05-08 21:06:35 +0800

    前端项目搭建

--- xueqiu/src/app/app-routing.module.ts
+++ xueqiu/src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// 导入组件
+import { IndexComponent } from './views/index/index.component';
+import { ChooseComponent } from './views/choose/choose.component';
+import { NewstockComponent } from './views/newstock/newstock.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent,
+  },
+  {
+    path: 'screener',
+    component: ChooseComponent,
+  },
+  {
+    path: 'newstock',
+    component: NewstockComponent,
+  },
+  // 404
+  {
+    path: '**',
+    component: NotfoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- xueqiu/src/app/app.module.ts
+++ xueqiu/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './views/index/index.component';
+import { ChooseComponent } from './views/choose/choose.component';
+import { NewstockComponent } from './views/newstock/newstock.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ChooseComponent,
+    NewstockComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
