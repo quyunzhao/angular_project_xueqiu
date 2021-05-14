285e6b2d997813936e3980551d24d17a2966e942
jingqu <1264352733@qq.com>
2021-05-14 21:47:36 +0800

    股票筛选前端数据获取

--- xueqiu/src/app/views/choose/choose.component.ts
+++ xueqiu/src/app/views/choose/choose.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewService } from 'src/app/service/new.service';
 
 @Component({
   selector: 'app-choose',
@@ -10,8 +11,28 @@ export class ChooseComponent implements OnInit {
   industriesList = [];
   // 地区
   areaList = [];
-
-  constructor() {}
+  areas = {};
+  constructor(public server: NewService) {
+    this.getIndustriesData();
+    this.getAreaData();
+  }
 
   ngOnInit(): void {}
+
+  // 获取行业数据
+  async getIndustriesData(): Promise<void> {
+    const result = await this.server.getIndustries();
+    // console.log(result.data);
+    this.industriesList = result.data.industries;
+    // console.log(this.industriesList);
+  }
+
+  // 获取区域数据
+  async getAreaData(): Promise<void> {
+    const result = await this.server.getArea();
+    this.areas = result.data.areas;
+    console.log(Object.keys(this.areas));
+    this.areaList = Object.keys(this.areas);
+    // console.log(this.areaList);
+  }
 }
